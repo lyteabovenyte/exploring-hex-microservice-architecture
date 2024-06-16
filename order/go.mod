9abe8afc5955d742dc9b3e4d3f93c0cd15c6403c
lyteatnyte <lyteatnyte@lyteatnyte.local>
2024-06-16 17:57:02 +0330

    added grpc service definition and grpc endpoint method 'Create' + config file to get the env variables

--- order/go.mod
+++ order/go.mod
@@ -3,9 +3,23 @@ module github.com/lyteabovenyte/microservices-main/order
 go 1.22.3
 
 require (
+	github.com/go-kit/kit v0.13.0
+	github.com/huseyinbabal/microservices-proto/golang/order v1.0.39
+	gorm.io/driver/mysql v1.5.7
+	gorm.io/gorm v1.25.10
+)
+
+require (
+	github.com/go-kit/log v0.2.0 // indirect
+	github.com/go-logfmt/logfmt v0.5.1 // indirect
 	github.com/go-sql-driver/mysql v1.7.0 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
-	gorm.io/driver/mysql v1.5.7 // indirect
-	gorm.io/gorm v1.25.10 // indirect
+	golang.org/x/net v0.0.0-20211216030914-fe4d6282115f // indirect
+	golang.org/x/sys v0.0.0-20220823224334-20c2bfdbfe24 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/genproto v0.0.0-20210917145530-b395a37504d4 // indirect
+	google.golang.org/grpc v1.43.0 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
 )
---
+++ order/internal/adapters/grpc/service.go
@@ -0,0 +1,41 @@
+package grpc
+
+import (
+	"fmt"
+	"google.golang.org/grpc/reflection"
+	"log"
+	"net"
+
+	"github.com/huseyinbabal/microservices-proto/golang/order"
+	"github.com/lyteabovenyte/microservices-main/order/internal/ports"
+	"google.golang.org/grpc"
+)
+
+type Adapter struct {
+	api                            ports.APIPort // core application dependency
+	port                           int           // port to serve grpc on
+	order.UnimplementedOrderServer               // for forward compatibility
+}
+
+func NewAdapter(api ports.APIPort, port int) *Adapter {
+	return &Adapter{api: api, port: port}
+}
+
+func (a Adapter) Run() {
+	var err error
+
+	listen, err := net.Listen("tcp", fmt.Sprintf(":%d", a.port))
+	if err != nil {
+		log.Fatalf("failed to listen to port %d, error: %v", a.port, err)
+	}
+
+	grpcServer := grpc.NewServer()
+	order.RegisterOrderServer(grpcServer, a)
+	// enable reflection to make grpcurl
+	if config.GetEnv() == "development" {
+		reflection.Register(grpcServer)
+	}
+	if err := grpcServer.Serve(listen); err != nil {
+		log.Fatalf("failed to serve grpc on port")
+	}
+}
