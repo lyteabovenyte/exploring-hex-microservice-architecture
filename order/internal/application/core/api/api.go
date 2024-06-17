01e228a056b39a6491e20c1e8149886a7ec4eb84
lyteabovenyte <lyteabovenyte@gmail.com>
2024-06-17 18:18:15 +0330

    added proper error handling for order serivce to charge a pament with invalid argument

--- order/internal/application/core/api/api.go
+++ order/internal/application/core/api/api.go
@@ -3,6 +3,9 @@ package api
 import (
 	"github.com/lyteabovenyte/microservices-main/order/internal/application/core/domain"
 	"github.com/lyteabovenyte/microservices-main/order/internal/ports"
+	"google.golang.org/genproto/googleapis/rpc/errdetails"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 // the API depends on DBPort
@@ -27,7 +30,17 @@ func (a *Application) PlaceOrder(order domain.Order) (domain.Order, error) {
 	}
 	paymentErr := a.payment.Charge(&order)
 	if paymentErr != nil {
-		return domain.Order{}, paymentErr
+		// Error with details
+		st, _ := status.FromError(paymentErr) // resolves status from a payment error
+		fieldErr := &errdetails.BadRequest_FieldViolation{
+			Field:       "payment",
+			Description: st.Message(),
+		}
+		badReq := &errdetails.BadRequest{}
+		badReq.FieldViolations = append(badReq.FieldViolations, fieldErr)
+		orderStatus := status.New(codes.InvalidArgument, "order creation failed")
+		statusWithDeatils, _ := orderStatus.WithDetails(badReq)
+		return domain.Order{}, statusWithDeatils.Err()
 	}
 	return order, nil
 }
