2873b45e4f03e9a0fe6bd5adf57be4e589aa1b8c
lyteabovenyte <lyteabovenyte@gmail.com>
2024-06-18 03:39:12 +0330

    implemented the payment service testing with status package

--- order/internal/application/core/api/api_test.go
+++ order/internal/application/core/api/api_test.go
@@ -2,11 +2,15 @@ package api
 
 import (
 	"context"
+	"errors"
 	"testing"
 
 	"github.com/lyteabovenyte/microservices-main/order/internal/application/core/domain"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
+	"google.golang.org/genproto/googleapis/rpc/errdetails"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type mockedPayment struct {
@@ -53,4 +57,26 @@ func TestPlaceOrder(t *testing.T) {
 	assert.Nil(t, err)
 }
 
+func Test_Should_Return_Error_When_Payment_Fail(t *testing.T) {
+	payment := new(mockedPayment)
+	db := new(mockedDb)
+	payment.On("Charge", mock.Anything).Return(errors.New("insufficient balance"))
+	db.On("Save", mock.Anything).Return(nil)
 
+	application := NewApplication(db, payment)
+	_, err := application.PlaceOrder(domain.Order{
+		CustomerID: 123,
+		OrderItems: []domain.OrderItem{
+			{
+				ProductCode: "bag",
+				UnitPrice:   2.5,
+				Quantity:    6,
+			},
+		},
+		CreatedAt: 0,
+	})
+	st, _ := status.FromError(err)
+	assert.Equal(t, st.Message(), "order creation failed")
+	assert.Equal(t, st.Details()[0].(*errdetails.BadRequest).FieldViolations[0].Description, "insufficient balance")
+	assert.Equal(t, st.Code(), codes.InvalidArgument)
+}
